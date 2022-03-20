40c95405abed154da2c4f0d593840c68b8404759
Veronika Novákiová <56062843+103071@users.noreply.github.com>
2022-03-20 18:28:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -41,7 +41,72 @@ def do_stuff():
     return json_string
 
 # Zadanie 2
+# 2
+@app.route('/v2/players/<string:player_id>/game_exp/', methods=['GET'])
+def get_game_exp():
+    pass
+
 # 1
+@app.route('/v2/patches', methods=['GET'])
+def get_patches():
+    conn = connect()
+
+    cur = conn.cursor()
+    cur.execute("select vsetkypece.patch_version, vsetkypece.patch_start_date, vsetkypece.patch_end_date, matches.id as match_id, duration "
+                "from matches "
+                "left join ("
+                "select patches.name as patch_version, "
+                "extract(epoch from patches.release_date) as patch_start_date, "
+                "extract (epoch from next_patch.release_date) as patch_end_date "
+                "from patches "
+                "left join patches as next_patch "
+                "on patches.id = next_patch.id  - 1 "
+                "order by patches.id"
+                ") as vsetkypece on (matches.start_time > vsetkypece.patch_start_date "
+                "and matches.start_time < coalesce (vsetkypece.patch_end_date, 9999999999))"
+                )
+
+    dic = {}
+    dic['patches'] = []
+
+    for column in cur:
+        current_patch = None
+
+
+        for patch in dic['patches']:
+            # column patch_version
+            if patch['patch_version'] == str(column[0]):
+                current_patch = patch
+                break
+
+        if current_patch is not None:
+            match = {}
+
+            match['match_id'] = column[3]
+            match['duration'] = column[4]
+
+            current_patch['matches'].append(match)
+
+
+        else:
+            current_patch = {}
+            current_patch['patch_version'] = column[0]
+            current_patch['patch_start_date'] = column[1]
+            current_patch['patch_end_date'] = column[2]
+
+            current_patch['matches'] = []
+            match = {}
+
+            match['match_id'] = column[3]
+            match['duration'] = column[4]
+
+            current_patch['matches'].append(match)
+            dic['patches'].append(current_patch)
+
+    json_string = json.dumps(dic)
+
+    return json_string
+
 
 
 def linux_version():
